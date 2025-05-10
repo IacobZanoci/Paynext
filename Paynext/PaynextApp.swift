ed89b9a607ebf5a67db195a90ab3cdef6154350c
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2025-05-10 21:54:19 +0300

    Initial Commit

---
+++ Paynext/PaynextApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaynextApp.swift
+//  Paynext
+//
+//  Created by Iacob Zanoci on 10.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PaynextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
