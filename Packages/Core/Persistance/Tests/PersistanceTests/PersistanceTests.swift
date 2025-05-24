812bfa22fecabfce21f4f7363dcd99736149c1b9
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2025-05-24 13:24:52 +0300

    test: Add 2 UnitTest cases for Persistance Model

--- Packages/Core/Persistance/Tests/PersistanceTests/PersistanceTests.swift
+++ Packages/Core/Persistance/Tests/PersistanceTests/PersistanceTests.swift
@@ -1,4 +1,43 @@
 import XCTest
 @testable import Persistance
 
-final class PersistanceTests: XCTestCase {}
+final class PersistanceTests: XCTestCase {
+    
+    // MARK: - Set Up
+    
+    var sut: CoreDataManager!
+    
+    override func setUp() {
+        super.setUp()
+        sut = CoreDataManager(inMemory: true)
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    // MARK: - Test Cases
+    
+    func test_givenUserEntityWithToken_whenSaveAndFetch_thenReturnsEntityWithSameToken() throws {
+        let entity = sut.create(type: User.self)
+        entity.token = "paynextToken"
+        try sut.saveContext()
+        
+        let result = try sut.fetch(type: User.self)
+        
+        XCTAssertEqual(result.count, 1)
+        XCTAssertEqual(result.first?.token, "paynextToken")
+    }
+    
+    func test_givenUserEntity_whenDeleteAndContextSave_thenEntityIsNotFethced() throws {
+        let entity = sut.create(type: User.self)
+        entity.token = "paynextToken"
+        sut.delete(entity)
+        try sut.saveContext()
+        
+        let result = try sut.fetch(type: User.self)
+        
+        XCTAssertEqual(result.count, 0)
+    }
+}
