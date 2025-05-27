c264a75bdb94608e2e1133612a270e9524a956cc
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2025-05-27 21:32:33 +0300

    feat: Implement  app navigation flow with Coordinator pattern

---
+++ Paynext/Coordinator/Route.swift
@@ -0,0 +1,16 @@
+//
+//  Route.swift
+//  Paynext
+//
+//  Created by Iacob Zanoci on 27.05.2025.
+//
+
+protocol Route: Hashable {}
+
+enum AppRoute: Route {
+    /// Define the Views for navigation
+    ///
+    /// Dashboard Views
+    /// case applyFilters
+    /// ...
+}
--- Paynext/PaynextApp.swift
+++ Paynext/PaynextApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PaynextApp: App {
     var body: some Scene {
         WindowGroup {
-            MainAppView()
+            MainView()
         }
     }
 }
