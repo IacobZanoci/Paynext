75a3fe14625e12dc48cce42fd96b352e6f034465
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2025-06-14 16:34:52 +0300

    feat: Add error message for validation & change max input length

--- Packages/Core/CredentialsValidator/Sources/CredentialsValidator/CredentialsValidator.swift
+++ Packages/Core/CredentialsValidator/Sources/CredentialsValidator/CredentialsValidator.swift
@@ -21,17 +21,17 @@ public class CredentialsValidator: CredentialsValidatorProtocol {
     }
     
     public func isAccountNumberValid(_ accountNumber: String) -> Bool {
-        let predicate = NSPredicate(format: "SELF MATCHES %@", "^[A-Z]{2}[0-9]{2}[A-Z]{2}[0-9]{18,24}$")
+        let predicate = NSPredicate(format: "SELF MATCHES %@", "^[A-Z]{2}[0-9]{2}[A-Z]{2}[0-9]{18,255}$")
         return predicate.evaluate(with: accountNumber)
     }
     
     public func isRoutingNumberValid(_ routingNumber: String) -> Bool {
-        let predicate = NSPredicate(format: "SELF MATCHES %@", "^[A-Z]{2}-[0-9]{7,9}$")
+        let predicate = NSPredicate(format: "SELF MATCHES %@", "^[A-Z]{2}-[0-9]{7,255}$")
         return predicate.evaluate(with: routingNumber)
     }
     
     public func isAmountValid(_ amount: String) -> Bool {
-        let predicate = NSPredicate(format: "SELF MATCHES %@", #"^\d+(\.\d{2})?$"#)
+        let predicate = NSPredicate(format: "SELF MATCHES %@", #"^\d{1,10}(\.\d{1,2})?$"#)
         return predicate.evaluate(with: amount)
     }
 }
